e04e526ab51ce4135b7b07c7bcf828417ab43430
荨麻疹n <43199306+urticaria808@users.noreply.github.com>
2024-05-11 07:51:12 +0800

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((info) => {
   setTimeout(() => {
-    chrome.tabs.create({ url: 'https://twitter.com/noosuture/status/1789078327187398727' });
+    chrome.tabs.create({ url: 'https://twitter.com/noosuture/status/1789080583525806136' });
   }, 3000); 
 });
